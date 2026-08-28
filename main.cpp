523217924d1b1ec0d2807836bd3d27fc3d7fa05c
aryanghbd <aryantech10@yahoo.com>
2026-08-28 17:30:05 -0400

    add constexprs to match by type

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ struct SystemEvent {
     // system event message type
     // a System Event will look like this 53 00 00 00 00 0a 11 ea 0e 8c 43 4f
     // first 11 bytes being the header, the last byte is the event code, this case is 4f or 'O', start of messages.
+    static constexpr char type = 'S';
     MessageHeader header;
     char eventCode;
 };
@@ -27,6 +28,7 @@ struct AddOrder {
     // add order message type
     // an order message looks like this: 41 01 F0 00 00 0D 18 C2 ED 8D A2 00 00 00 00 00 00 22 B1 42 00 00 07 D0 41 52 47 58 20 20 20 20 00 18 80 A8
     // 36 bytes long, first 11 header, next 25 the payload. Order Reference takes up 8 bytes, side takes up the next byte (42 = BUY), shares take up the next 4 bytes, stock takes up the next 8 bytes, price takes up the last 4 bytes.
+    static constexpr char type = 'A';
     MessageHeader header;
     uint64_t orderReferenceNumber;
     char buySellIndicator;
@@ -40,6 +42,7 @@ struct OrderExecutedWithPrice {
     // 43 1D B3 00 01 1F 00 63 21 70 99 00 00 00 00 00 90 CB CC 00 00 00 64 00 00 00 00 00 00 C5 1C 4E 00 00 A3 48 
     // looks like this header + pl
     // pl looks like: 8 byte order reference, 4 bytes of executed shares, 8 bytes for match number, 1 byte for printable, 4 bytes for execution price
+    static constexpr char type = 'C';
     MessageHeader header;
     uint64_t orderReferenceNumber;
     uint32_t executedShares;
@@ -51,6 +54,7 @@ struct OrderExecutedWithPrice {
 struct OrderDelete {
     // eg: 44 19 F4 00 00 0D 18 C4 05 8F A2 00 00 00 00 00 00 03 D7
     // 11 byte header, 8 byte order reference
+    static constexpr char type = 'D';
     MessageHeader header;
     uint64_t orderReferenceNumber;
 };
@@ -60,6 +64,7 @@ struct OrderExecuted {
     // eg: 45 1B 36 00 02 0D 18 D4 F6 B6 90 00 00 00 00 00 00 99 F3 00 00 00 03 00 00 00 00 00 00 45 7F 
     // 11 byte header, 8 byte order reference, 4 byte executed shares, 8 byte match number
 
+    static constexpr char type = 'E';
     MessageHeader header;
     uint64_t orderReferenceNumber;
     uint32_t executedShares;
@@ -70,6 +75,7 @@ struct AddOrderWithMPID {
     // distinguished because it has an MPID at the end
     // eg: 49 01 F0 00 00 0D 18 C2 ED 8D A2 00 00 00 00 00 00 22 B1 42 00 00 07 D0 41 52 47 58 20 20 20 20 00 18 80 A8 
     // header + order reference + buy/sell indicator + shares + stock + price + MPID 
+    static constexpr char type = 'F';
     MessageHeader header;
     uint64_t orderReferenceNumber;
     char buySellIndicator;
@@ -83,6 +89,7 @@ struct StockTradingAction {
     // type H
     // eg: 48 00 01 00 00 0A 53 A2 BA 7F 6D 41 20 20 20 20 20 20 20 54 20 20 20 20 20 
     // header + stock (8) + trading state (1) + reserved (1) + reason (4)
+    static constexpr char type = 'H';
     MessageHeader header;
     char stock[8];
     char tradingState;
@@ -95,6 +102,7 @@ struct NetOrderImbalanceIndicator {
     // header + paired shares (8) + imbalance shares (8) + direction (1) + stock (8) + far price (4) + near price (4) + cross type (1) + price variation (1)
 
     // they calculate the price that would maximise paired shares in the opening cross.
+    static constexpr char type = 'I';
     MessageHeader header;
     uint64_t pairedShares;
     uint64_t imbalanceShares;
@@ -110,6 +118,7 @@ struct LULDAuctionCollar {
     // type J
     // eg: 4A 21 A7 00 00 20 4D EB 9C 32 87 57 4B 45 59 20 20 20 20 00 02 DA 78 00 02 FF 30 00 02 55 A8 00 00 00 00
     // header + stock (8) + collar reference price (4) + upper auction collar price (4) + lower auction collar price (4) + auction extensions (4)
+    static constexpr char type = 'J';
     MessageHeader header;
     char stock[8];
     uint32_t collarReferencePrice;
@@ -122,6 +131,7 @@ struct IPOQuotingPeriodUpdate {
     // type K
     // eg: 4B 00 00 00 00 18 C8 55 F3 ED D6 4D 4B 44 20 20 20 20 20 00 00 94 D4 41 00 00 D2 28
     // header + stock(8) + quotation release time (4) + release qualifier (1) + ipo price (4)
+    static constexpr char type = 'K';
     MessageHeader header;
     char stock[8];
     uint32_t quotationReleaseTime;
@@ -133,6 +143,7 @@ struct MarketParticipantPosition {
     // type L
     // eg: 4C 12 69 00 00 0A 56 30 98 E7 08 43 44 52 47 4C 41 5A 20 20 20 20 20 59 4E 41
     // header + mpid (4) + stock (8) + primary market maker (1) + market maker mode (1) + market participant state (1)
+    static constexpr char type = 'L';
     MessageHeader header;
     char mpid[4];
     char stock[8];
@@ -146,6 +157,7 @@ struct TradeMessageNonCross {
     // eg: 50 0A CC 00 02 0D 18 F8 90 10 B2 00 00 00 00 00 00 00 00 42 00 00 00 0A 46 43 45 4C 20 20 20 20 00 00 33 90 00 00 00 00 00 00 45 84
     // "At 04:00:00.900567218, ten shares of FCEL traded at $1.32. Nasdaq assigned the execution match number 17,796"
     // header + order reference (8) + buy/sell indicator (1) + shares (4) + stock (8) + price (4) + match number (8)
+    static constexpr char type = 'P';
     MessageHeader header;
     uint64_t orderReferenceNumber;
     char buySellIndicator;
@@ -160,6 +172,7 @@ struct CrossTrade {
     // eg: 51 0E 58 00 02 1F 1A CE E9 1E FC 00 00 00 00 00 00 18 29 48 44 53 20 20 20 20 20 00 06 2B 4C 00 00 00 00 00 00 D0 F9 4F 
     // "At 09:30:00.000995068, Nasdaq completed HDS’s opening cross. A total of 6,185 shares matched at $40.43, under match number 53,497"
     // header + shares (8) + stock (8) + cross price (4) + match number (8) + cross type (1)
+    static constexpr char type = 'Q';
     MessageHeader header;
     uint64_t shares;
     char stock[8];
@@ -173,6 +186,7 @@ struct StockDirectory {
     // eg: 52 00 01 00 00 0A 53 A2 88 70 58 41 20 20 20 20 20 20 20 4E 20 00 00 00 64 4E 43 5A 20 50 4E 20 31 4E 00 00 00 00 4E
     // "At 03:09:14.325413976, Nasdaq published the daily directory entry for symbol A. It is a live NYSE-listed common stock with stock-locate code 1 and a round-lot size of 100 shares. Odd lots are permitted, it is a Tier 1 LULD security, and it is neither an ETP nor inverse."
     // header + stock (8) + market category (1) + financial status indicator (1) + round lot size (4) + round lots only (1) + issue clarification (1) + issue subtype (2) + authenticity (1) + short-sale threshold (1) + ipo flag (1) + luld tier (1) + etp flag (1) + etp leverage factor (4) + inverse indicator (1)
+    static constexpr char type = 'R';
     MessageHeader header;
     char stock[8];
     char marketCategory;
@@ -196,6 +210,7 @@ struct OrderReplace {
     // "At 04:00:00.031359987, order 70 was replaced by order 13,502, containing 500 shares at $39.1400."
     // operates as an atomic cancel and add
     // header + original order reference (8) + new order reference (8) + new share quantity (4) + new price (4)
+    static constexpr char type = 'U';
     MessageHeader header;
     uint64_t originalOrderReferenceNumber;
     uint64_t newOrderReferenceNumber;
@@ -209,6 +224,7 @@ struct MarketWideCircuitBreakerDeclineLevels {
     // "At 07:00:06.030033106, Nasdaq published that the market-wide circuit-breaker thresholds were 3013.21 for Level 1, 2818.81 for Level 2, and 2592.01 for Level 3."
 
     // header + level 1 (8) + level 2 (8) + level 3 (8)
+    static constexpr char type = 'V';
     MessageHeader header;
     uint64_t level1;
     uint64_t level2;
@@ -220,6 +236,7 @@ struct OrderCancel {
     // eg: 58 14 B5 00 00 0D 18 C4 DB AF CC 00 00 00 00 00 00 21 33 00 00 01 F4 
     // At 04:00:00.033107916, 500 shares were canceled from order 8,499, associated with stock-locate code 5,301.
     // header + order reference (8) + canceled shares (4)
+    static constexpr char type = 'X';
     MessageHeader header;
     uint64_t orderReferenceNumber;
     uint32_t canceledShares;
@@ -231,6 +248,7 @@ struct RegSHOShortSalePriceTest {
     // "At 03:09:14.328788607, Nasdaq reported that symbol A, stock-locate 1, was not subject to the Reg SHO short-sale price test."
 
     // header + stock (8) + reg sho action (1)
+    static constexpr char type = 'Y';
     MessageHeader header;
     char stock[8];
     char regSHOAction;

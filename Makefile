f80ea6aa74d094db1ad1ff24ede37da79a237d02
aryanghbd <aryantech10@yahoo.com>
2026-08-28 06:07:04 -0400

    faffing around with the itch file

--- Makefile
+++ Makefile
@@ -4,18 +4,19 @@ CXXFLAGS ?= -std=c++20 -Wall -Wextra -Wpedantic
 TARGET := build/itch-lob
 SOURCE := main.cpp
 OBJECT := build/main.o
+LDLIBS := -lz
 
 .PHONY: all run clean
 
 all: $(TARGET)
 
 $(TARGET): $(OBJECT)
-	$(CXX) $(CXXFLAGS) $^ -o $@
+	$(CXX) $(CXXFLAGS) $^ -o $@ $(LDLIBS)
 
 $(OBJECT): $(SOURCE)
 	@mkdir -p $(dir $@)
 	$(CXX) $(CXXFLAGS) -c $< -o $@
-
+ 
 run: $(TARGET)
 	./$(TARGET)
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <zlib.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "itch-lob: project shell is working\n";
+    gzFile file = gzopen("data/12302019.NASDAQ_ITCH50.gz", "rb");
+    // now read the contents of the file using gzread
+    array<unsigned char, 1000 * 1024> buffer{};
+    int bytesRead = gzread(file, buffer.data(), buffer.size()); // Read into the buffer
+    if (!file) {
+        cerr << "Failed to open gzip file\n";
+        return 1;
+    }
+    gzclose(file);
+    cout << "Successfully read " << bytesRead << " bytes\n";
+
+    // try to read the first 32 bytes
+
+    for (int i = 0; i < (64 * 1024) && i < bytesRead; ++i) {
+        cout << hex << static_cast<int>(buffer[i]) << " ";
+    }
+    cout << endl;
+    // free the buffer memory
+    free(buffer.data());
     return 0;
 }
